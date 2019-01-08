0e008ac72429928219636bb3967b23d8e0055235
Leandro <asengardeons@hotmail.com>
2019-01-08 16:29:12 -0200

    Added action to add, edit and remove albums

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -10,16 +10,28 @@ namespace Application;
 use Zend\Router\Http\Literal;
 use Zend\Router\Http\Segment;
 use Zend\ServiceManager\Factory\InvokableFactory;
+use Album\Controller\AlbumController;
+
 
 return [
     'router' => [
         'routes' => [
+            // 'home' => [
+            //     'type' => Literal::class,
+            //     'options' => [
+            //         'route'    => '/',
+            //         'defaults' => [
+            //             'controller' => Controller\IndexController::class,
+            //             'action'     => 'index',
+            //         ],
+            //     ],
+            // ],
             'home' => [
-                'type' => Literal::class,
+                'type' => \Zend\Router\Http\Literal::class,
                 'options' => [
                     'route'    => '/',
                     'defaults' => [
-                        'controller' => Controller\IndexController::class,
+                        'controller' => AlbumController::class, // <-- change here
                         'action'     => 'index',
                     ],
                 ],
